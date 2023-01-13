b6adaca2c9102270ac2b010a0504db38f248edb0
navas <gabnavas.dev@gmail.com>
2023-01-13 17:38:43 -0300

    Update Parking Spot

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -73,6 +74,19 @@ public class ParkingSpotController {
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found: Parking Spot not found."); 
     }
     parkingSpotService.delete(parkingSpotFound.get());
-    return ResponseEntity.status(HttpStatus.NO_CONTENT).body(parkingSpotFound);
+    return ResponseEntity.status(HttpStatus.OK).body(parkingSpotFound);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Object> updateParkingSpot(@PathVariable UUID id, @RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+    Optional<ParkingSpotModel> parkingSpotFound = parkingSpotService.findById(id);
+    if(parkingSpotFound.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found: Parking Spot not found."); 
+    }
+    var parkingSpotModel = new ParkingSpotModel();
+    BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+    parkingSpotModel.setId(parkingSpotFound.get().getId());
+    parkingSpotModel.setRegistrationDate(parkingSpotFound.get().getRegistrationDate());
+    return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
   }
 }
