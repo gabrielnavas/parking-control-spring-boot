72dfc70cb84a1bbbd4052d82dffd5e0c84e41dbe
navas <gabnavas.dev@gmail.com>
2023-01-13 18:08:31 -0300

    add pagination in find all Parking Spot

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -8,6 +8,9 @@ import java.util.UUID;
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Sort;
 
 import com.api.parkingcontrol.ParkingSpotDto;
 import com.api.parkingcontrol.models.ParkingSpotModel;
@@ -54,8 +58,10 @@ public class ParkingSpotController {
   }
 
   @GetMapping()
-  public ResponseEntity<Object> getAllParkingSpots() {
-    return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
+  public ResponseEntity<Page<ParkingSpotModel>> getAllParkingSpots(
+    @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable
+  ) {
+    return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll(pageable));
   }
 
   @GetMapping("/{id}")
